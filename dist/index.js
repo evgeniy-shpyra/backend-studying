c691407fa472b1fdcf3f0846de99acb8b0999026
evgeniy-shpyra <foksmix256@gmail.com>
2023-02-05 18:58:35 +0200

    09 lesson

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,62 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send({ message: "Hello World!1111" });
+const db = {
+    courses: [
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
+        { id: 3, title: "qa" },
+    ],
+};
+const jsonBodyMiddleware = express_1.default.json();
+app.use(jsonBodyMiddleware);
+app.get("/courses", (req, res) => {
+    let foundCourse = db.courses;
+    if (req.query.title)
+        foundCourse = foundCourse.filter((item) => item.title.indexOf(req.query.title) > -1);
+    res.json(foundCourse);
+});
+app.get("/courses/:id", (req, res) => {
+    const foundCourse = db.courses.find((item) => item.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    res.json(foundCourse);
+});
+app.post("/courses", (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400);
+        return;
+    }
+    const newCourse = {
+        id: +new Date(),
+        title: req.body.title.trim(),
+    };
+    db.courses.push(newCourse);
+    res.status(201).json(newCourse);
+});
+app.delete("/courses/:id", (req, res) => {
+    const indexOfCourse = db.courses.findIndex((item) => item.id === +req.params.id);
+    if (indexOfCourse < 0) {
+        res.sendStatus(404);
+        return;
+    }
+    db.courses.splice(indexOfCourse, 1);
+    res.sendStatus(204);
+});
+app.put("/courses/:id", (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400);
+        return;
+    }
+    const foundCourse = db.courses.find((item) => item.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    foundCourse.title = req.body.title.trim();
+    res.json(foundCourse);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,88 @@ import express from "express"
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-    res.send({ message: "Hello World!1111" })
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+}
+
+const db = {
+    courses: [
+        { id: 1, title: "front-end" },
+        { id: 2, title: "back-end" },
+        { id: 3, title: "qa" },
+    ],
+}
+
+const jsonBodyMiddleware = express.json()
+
+app.use(jsonBodyMiddleware)
+
+app.get("/courses", (req, res) => {
+    let foundCourse = db.courses
+
+    if (req.query.title)
+        foundCourse = foundCourse.filter(
+            (item) => item.title.indexOf(req.query.title as string) > -1
+        )
+
+    res.json(foundCourse)
+})
+app.get("/courses/:id", (req, res) => {
+    const foundCourse = db.courses.find((item) => item.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+
+    res.json(foundCourse)
+})
+app.post("/courses", (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
+    }
+    const newCourse = {
+        id: +new Date(),
+        title: req.body.title.trim(),
+    }
+    db.courses.push(newCourse)
+
+    res.status(HTTP_STATUSES.CREATED_201).json(newCourse)
+})
+app.delete("/courses/:id", (req, res) => {
+    const indexOfCourse = db.courses.findIndex(
+        (item) => item.id === +req.params.id
+    )
+    if (indexOfCourse < 0) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+    db.courses.splice(indexOfCourse, 1)
+
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+})
+app.put("/courses/:id", (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
+    }
+
+    const foundCourse = db.courses.find((item) => item.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+
+    foundCourse.title = req.body.title.trim()
+
+    res.status(204).json(foundCourse)
 })
 
 app.listen(port, () => {
